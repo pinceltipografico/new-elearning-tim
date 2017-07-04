bb743cf758d44a1ca0a3d1668ac47d8738d0850d
Renato Almeida <re.silva24@gmail.com>
2017-07-04 12:36:30 -0300

    Pagina de Introdução

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,77 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <main id="app">
+    <div class="image-background"></div>
+    <div class="logo">
+      <img src="~@/assets/svgs/logo-01.svg" alt="Tim"/>
+    </div>
     <router-view></router-view>
-  </div>
+  </main>
 </template>
-
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $fa-font-path:'../node_modules/font-awesome/fonts';
+  @import "scss/reset";
+  @import "scss/variables";
+  @import "scss/mixins";
+  @import "scss/commons";
+  @import "../node_modules/font-awesome/scss/font-awesome";
+  
+  html,
+  body{
+    width: 100%;
+    height: 100%;
+  }
+  
+  main {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+    background: #fff;
+    
+    .image-background{
+      display: block;
+      position: absolute;
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+      background: url("assets/backgrounds/home.jpg") no-repeat;
+      background-size: cover;
+    }
+    
+    .overlay{
+      position: absolute;
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+      background: rgba(#000,0.3);
+    }
+    
+    .logo{
+      position: absolute;
+      z-index: 3;
+      width: 120px;
+      top:40px;
+      left: 40px;
+    }
+    
+    section.page{
+      height: 100%;
+      width: 100%;
+      position: relative;
+      border:0 solid #fff;
+      z-index: 2;
+      animation: enterBorder $animationTime forwards;
+    }
+  }
+  
+  @keyframes enterBorder {
+    to{
+      border:15px solid #fff;
+    }
+  }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="page">
+    <div class="overlay"></div>
+    <div class="intro-title" v-bind:class="{'animation':animation}">
+      <span>Bem-vindo ao e-learning</span>
+      <hr>
+      <h1>Customer</h1>
+      <h1>experience</h1>
+      <hr>
+      <div class="buttons">
+        <span>iniciar</span>
+        <i class="fa fa-play-circle"></i>
+      </div>
+    </div>
+  </section>
 </template>
-
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        animation: false,
+        blinkButton: false
+      }
+    },
+    mounted () {
+      setTimeout(function () {
+        this.animation = true
+      }.bind(this), 2000)
     }
   }
-}
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="scss">
+  @import "../scss/variables";
+  @import "../scss/mixins";
+  
+  .intro-title {
+    position: absolute;
+    left: 50%;
+    bottom: 70px;
+    text-transform: uppercase;
+    text-align: center;
+    transform: translateX(-50%);
+    z-index: 3;
+    
+    *{
+      transition: all $animationTime;
+    }
+    
+    hr{
+      opacity: 0;
+      &:nth-of-type(1){
+        transform: translateX(-100%);
+      }
+      &:nth-of-type(2){
+        transform: translateX(100%);
+      }
+    }
+    
+    h1,
+    span {
+      color: #fff;
+    }
+    > span {
+      display: inline-block;
+      @include font-size(2);
+      transform: scaleY(0);
+    }
+    h1 {
+      @include font-size(5);
+      margin: 0;
+      font-weight: 300;
+      text-align: left;
+      padding: 0 40px;
+      opacity: 0;
+      transition-delay: 0.4s;
+    }
+    h1:nth-of-type(1) {
+      @include font-size(5.4);
+      color: $brand-details;
+    }
+    h1:nth-of-type(2) {
+      margin-top: -15px;
+    }
+    
+    .buttons{
+      transition-delay: 1s;
+      transform: translateY(100%);
+      opacity: 0;
+    }
+    
+    &.animation {
+      > span {
+        transform: scaleY(1);
+      }
+      hr{
+        opacity: 1;
+        transform: translateX(0);
+      }
+      h1{
+        opacity: 1;
+      }
+      .buttons{
+        transform: translateY(0);
+        opacity: 1;
+        animation-delay: 1s;
+        border:2px solid #fff;
+        animation: blinkButton 1s infinite;
+      }
+    }
+  }
+  @keyframes blinkButton {
+    0%{
+      border:2px solid #fff;
+    }
+    50%{
+      border:2px solid $brand-details;
+    }
+    100%{
+      border:2px solid #fff;
+    }
+  }
 </style>
