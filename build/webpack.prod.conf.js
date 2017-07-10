942b956c5e7357c9955f31cd42c8302009808a96
Renato Almeida <re.silva24@gmail.com>
2017-07-09 23:14:24 -0300

    Pagina 15

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -41,7 +41,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
-      publicPath: '/'
+      publicPath: '../../'
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
