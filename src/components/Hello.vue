87fbf4b63cc963d261b4dbebee8cbca508d7d401
Renato Almeida <re.silva24@gmail.com>
2017-12-12 17:47:55 -0200

    Configurando deploy

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -39,7 +39,7 @@
       setTimeout(function () {
         this.animation = true
       }.bind(this), 1000)
-      console.log('heu')
+//      console.log('heu')
     },
     /**
      | ----------------------------------------------
